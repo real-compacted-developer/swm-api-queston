569994b6ac48bd4dec2519b5e3283b0d24a10772
changicho <changicho29@gmail.com>
2020-09-22 16:58:36 +0900

    feat: 질문 타입 정의 추가
    설계한 질문 타입의 정보를 담당하는 Interface 파일을 추가함

---
+++ src/types/IQuestionInfo.ts
@@ -0,0 +1,19 @@
+interface IQuestionInfo {
+  id: number; // 유니크한 id 값
+
+  userInfo: {
+    userName: string;
+    profileImageURL: string;
+  };
+
+  slideInfo: {
+    page: number; // 연관된 슬라이드
+    imageURL: string; // 연관된 슬라이드 이미지
+  };
+
+  like: number; // 좋아요 수
+
+  content: string; // 질문 내용
+}
+
+export default IQuestionInfo;
