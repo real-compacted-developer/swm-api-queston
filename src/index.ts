11af01e8ac35fbdcadadc5cba44cbe7d5e0e3bbc
changicho <changicho29@gmail.com>
2020-09-22 16:47:07 +0900

    chore: 특정 환경으로 실행하는 명령어 추가
    package.json에 누락되어있던 License 항목 추가.
    이는 node로 실행시킬 때 경고문을 없애기 위함.
    
    test 환경과 배포 환경으로 실행하는 명령을 각각 추가함.
    또한 실행할 때 콘솔에 해당 내용을 출력하도록 설정.
    
    배포 서버에는 콘솔 명령이 필요하지 않으므로 이를 삭제

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,11 @@ import app from "./server";
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
+if (process.env.NODE_ENV === "development") {
+  console.log("listen port", process.env.PORT || 3000);
+}
+if (process.env.NODE_ENV === "test") {
+  console.log("test mode. the port is", process.env.PORT || 3000);
+}
+
 app.listen(port);
