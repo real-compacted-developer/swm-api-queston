1ac266cb80dcc34d15a9fd1a6b017e11c5f0a587
changicho <changicho29@gmail.com>
2020-09-23 17:03:59 +0900

    feat: 질문 데이터 조회 기능 추가 (test, mock data)
    테스트 환경에서 dummy fetch를 이용해 질문 데이터를 가져오는
    코드 추가
    
    질문 내용마다 유저정보를 더해 보내주는 기능을 추가해야함

--- src/routes/question.ts
+++ src/routes/question.ts
@@ -1,9 +1,71 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body, validationResult, buildCheckFunction } from "express-validator";
+
+import { DBLayerQuestion } from "../fetch";
+import { IQuestionInfo } from "../types";
+
+const checkParamAndQuery = buildCheckFunction(["params", "query"]);
 
 const router = Router();
 
-router.get("/echo", (req, res) => {
-  res.send("echo");
-});
+type SuccessResponse = {
+  success: true;
+  message: string;
+  data: {
+    questions: IQuestionInfo[];
+  };
+};
+
+type FailResponse = {
+  success: false;
+  message: string;
+};
+
+/**
+ * @api {get} /study/:roomNumber/questions?sort_by=created&order=asc 방의 질문 목록을 가져옴
+ * @apiName GetQuestions
+ * @apiGroup Question
+ *
+ * @apiParam {String} roomNumber 유일한 방 번호
+ *
+ * @apiSuccess {Boolean} success API 호출 성공 여부
+ * @apiSuccess {String} message 응답 메시지
+ * @apiSuccess {Object} data 해당 방의 질문 리스트
+ */
+router.get(
+  "/study/:roomNumber/questions",
+  [checkParamAndQuery("roomNumber").isString()],
+  async (req: Request, res: Response) => {
+    if (!validationResult(req).isEmpty()) {
+      const responseJSON: FailResponse = {
+        success: false,
+        message: "INVALID URL (PARAMETERS)",
+      };
+
+      res.status(500).send(responseJSON);
+      return;
+    }
+
+    const { roomNumber } = req.params;
+    const { sort_by, order } = req.query;
+
+    const query = {
+      sortBy: sort_by,
+      orderBy: order,
+    };
+
+    const questionData = await DBLayerQuestion.default(roomNumber, query);
+
+    const responseJSON: SuccessResponse = {
+      success: true,
+      message: "SUCCESS",
+      data: {
+        questions: questionData,
+      },
+    };
+
+    res.status(200).send(responseJSON);
+  }
+);
 
 export default router;
