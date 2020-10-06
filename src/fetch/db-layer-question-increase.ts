13f14ac4cbedc4d7817135ad1b6401b69d3b7438
changicho <changicho29@gmail.com>
2020-10-06 18:47:05 +0900

    feat: 질문 좋아요 증감 fetch 함수 추가
    ?: 증가, 감소 함수를 각각 생성함. 이는 DB Layer에서 method별로 구
    별해서 사용하기 때문

---
+++ src/fetch/db-layer-question-increase.ts
@@ -0,0 +1,42 @@
+import { IQuestionRow } from "src/types";
+import { dummyQuestionRow } from "../dummy";
+
+import axios from "axios";
+
+type Option = {
+  diff: 1 | -1;
+};
+
+async function testConnection(questionId: string) {
+  const dummyData: IQuestionRow = JSON.parse(JSON.stringify(dummyQuestionRow))
+    .default[0];
+
+  dummyData.like += 1;
+
+  return dummyData;
+}
+
+async function fetchData(questionId: string) {
+  try {
+    const response = await axios
+      .post(`${process.env.DB_LAYER_HOST}/question/like/${questionId}`)
+      .then((response) => response.data);
+
+    const { data, success } = response;
+    if (!success) return undefined;
+
+    return data as IQuestionRow;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+export let fetchQuestion: (
+  questionId: string
+) => Promise<IQuestionRow | undefined>;
+
+if (process.env.NODE_ENV === "test") {
+  fetchQuestion = testConnection;
+} else {
+  fetchQuestion = fetchData;
+}
