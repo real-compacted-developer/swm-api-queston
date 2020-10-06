cee30f1bd3f78b018270beea2a809e5f524fa180
changicho <changicho29@gmail.com>
2020-10-06 18:48:35 +0900

    feat: 질문 좋아요 변경 API 추가
    ?: 불필요한 코드 제거 및 새로운 API 추가

--- src/routes/question.ts
+++ src/routes/question.ts
@@ -1,27 +1,11 @@
-import { Router, Request, Response } from "express";
-import { body, validationResult, buildCheckFunction } from "express-validator";
+import { Router } from "express";
+import { buildCheckFunction } from "express-validator";
 
-import { DBLayerQuestion, DBLayerUser } from "../fetch";
-import { IQuestionInfo, IUserRow } from "../types";
+import { getQuestions, updateLike } from "./middleware";
 
 const checkParamAndQuery = buildCheckFunction(["params", "query"]);
 
-import { getQuestions } from "./middleware";
-
 const router = Router();
-
-type SuccessResponse = {
-  success: true;
-  message: string;
-  data: {
-    questions: IQuestionInfo[];
-  };
-};
-
-type FailResponse = {
-  success: false;
-  message: string;
-};
 
 /**
  * @api {get} /study/:roomNumber/questions?sort_by=created&order_by=asc 방의 질문 목록을 가져옴
@@ -36,8 +20,26 @@ type FailResponse = {
  */
 router.get(
   "/study/:roomNumber/questions",
-  [checkParamAndQuery("roomNumber").isNumeric()],
-  getQuestions.default
+  [checkParamAndQuery("roomNumber").isString()],
+  getQuestions
+);
+
+/**
+ * @api {patch} /question/:questionId?type=increase 방의 질문의 좋아요를 변화시킴 (1개씩)
+ * @apiName IncreaseLike
+ * @apiGroup Question
+ *
+ * @apiParam {String} questionId 유일한 질문 번호
+ * @apiParam {String} type 좋아요의 증감 "increase" | "decrease"
+ *
+ * @apiSuccess {Boolean} success API 호출 성공 여부
+ * @apiSuccess {String} message 응답 메시지
+ * @apiSuccess {Object} data 좋아요를 증가시킨 질문 정보
+ */
+router.patch(
+  "/question/:questionId",
+  [checkParamAndQuery("questionId").isString()],
+  updateLike
 );
 
 export default router;
