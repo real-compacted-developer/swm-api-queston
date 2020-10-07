9434eaddfe6ddacff945e2d6d742b2095696a355
changicho <changicho29@gmail.com>
2020-10-07 16:48:56 +0900

    feat: DB Layer에서 유저정보를 가져오는 코드 추가
    유저 데이터 타입이 변경되어, key 값을 수정함
    axios로 DB Layer에 fetch를 수행하는 부분 추가

--- src/fetch/db-layer-users.ts
+++ src/fetch/db-layer-users.ts
@@ -1,31 +1,28 @@
+import axios from "axios";
+
 import { IUserRow } from "src/types";
 import { dummyUserRow } from "../dummy";
 
-async function testConnection(userId: number) {
+async function testConnection(userId: string): Promise<IUserRow | undefined> {
   const dummyData: IUserRow[] = JSON.parse(JSON.stringify(dummyUserRow))
     .default;
 
   const user = dummyData.find((cur) => {
-    if (cur.userId === userId) return cur;
+    if (cur.id === userId) return cur;
   });
 
   return user;
 }
 
-async function fetch(userId: number) {
-  const dummyData: IUserRow[] = JSON.parse(JSON.stringify(dummyUserRow))
-    .default;
+async function fetch(userId: string): Promise<IUserRow | undefined> {
+  const userData = await axios
+    .get(`${process.env.DB_LAYER_HOST}/user/${userId}`)
+    .then((response) => response.data);
 
-  const user = dummyData.find((cur) => {
-    if (cur.userId === userId) return cur;
-  });
-
-  await setTimeout(() => {}, 500);
-
-  return user;
+  return userData;
 }
 
-export let fetchUser: (userId: number) => Promise<IUserRow | undefined>;
+export let fetchUser: (userId: string) => Promise<IUserRow | undefined>;
 
 if (process.env.NODE_ENV === "test") {
   fetchUser = testConnection;
